9c911e9b1a87ffedbde68bb64d5859ae4d671402
Rakibul <rakibuliuict@gmail.com>
2025-07-01 23:40:01 +0600

    split_all in test.py

--- PICAI_SSL/utils/test_3d_patch.py
+++ PICAI_SSL/utils/test_3d_patch.py
@@ -146,7 +146,7 @@ def test_single_case_plus(model1, model2, image, stride_xy, stride_z, patch_size
 
 def var_all_case_LA(model, num_classes, patch_size=(256, 256, 16), stride_xy=18, stride_z=4):
     """Evaluate the model on all test cases and return the average Dice score."""
-    with open('/content/drive/MyDrive/SSL/SSL_Project_code_Colab2/PICAI_SSL/Datasets/picai/data_split/test.txt', 'r') as f:
+    with open('/content/drive/MyDrive/SSL/SSL_Project_code_Colab2/PICAI_SSL/Datasets/picai/data_split_all/test.txt', 'r') as f:
         image_list = f.readlines()
 
     image_list = [os.path.join('/content/drive/MyDrive/SSL/Dataset/PICAI_dataset/', item.strip(), f"{item.strip()}.h5") for item in image_list]
@@ -176,7 +176,7 @@ def var_all_case_LA(model, num_classes, patch_size=(256, 256, 16), stride_xy=18,
     return avg_dice
 
 def var_all_case_LA_mean(model1, model2, num_classes, patch_size=(256, 256, 16), stride_xy=18, stride_z=4):
-    with open('/content/drive/MyDrive/SSL/SSL_Project_code_Colab2/PICAI_SSL/Datasets/picai/data_split/test.txt', 'r') as f:
+    with open('/content/drive/MyDrive/SSL/SSL_Project_code_Colab2/PICAI_SSL/Datasets/picai/data_split_all/test.txt', 'r') as f:
         image_list = f.readlines()
     image_list = ["/content/drive/MyDrive/SSL/Dataset/PICAI_dataset/" + item.strip() + "/" + item.strip() + ".h5" for item in image_list]
     loader = tqdm(image_list)
@@ -191,7 +191,7 @@ def var_all_case_LA_mean(model1, model2, num_classes, patch_size=(256, 256, 16),
     return avg_dice
 
 def var_all_case_LA_plus(model1, model2, num_classes, patch_size=(256, 256, 16), stride_xy=18, stride_z=4):
-    with open('/content/drive/MyDrive/SSL/SSL_Project_code_Colab2/PICAI_SSL/Datasets/picai/data_split/test.txt', 'r') as f:
+    with open('/content/drive/MyDrive/SSL/SSL_Project_code_Colab2/PICAI_SSL/Datasets/picai/data_split_all/test.txt', 'r') as f:
         image_list = f.readlines()
     image_list = ["/content/drive/MyDrive/SSL/Dataset/PICAI_dataset/" + item.strip() + "/" + item.strip() + ".h5" for item in image_list]
     loader = tqdm(image_list)
